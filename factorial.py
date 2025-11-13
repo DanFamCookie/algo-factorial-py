6e1a2a37b3d91229fcfc998604a872cf02fc00de
YOUR_NAME <YOUR_EMAIL>
2025-11-12 18:26:02 -0800

    Danny Segovia 11/12/25 Homework finsished code

--- factorial.py
+++ factorial.py
@@ -1,3 +1,21 @@
+# factorial is a number times its decending number so factorial(10) is 10*9*8*7*6*5*4*3*2*1
+
+
+
+
+
+
+
 def factorial(num):
-	# your code here
+	result = 1
+	for i in range(1, num + 1):
+		result *= i
+	return result
+	
+
+	
+		
 	pass
+
+
+print(factorial(100))
